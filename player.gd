98f0dd693c2b06b4458e32309cd153fea2527c6e
Saph <lc23-0425@lclark.edu>
2026-07-01 12:46:50 -0700

    Added text to speech

--- player.gd
+++ player.gd
@@ -4,6 +4,10 @@ const SPEED = 5.0
 const ROTATION_SPEED = 1.5
 
 var rotation_direction = 0
+var degrees = 0
+
+var voices := DisplayServer.tts_get_voices_for_language("en")
+var voice_id := voices[0]
 
 func _physics_process(delta: float) -> void:
 	var rotation_input = Input.get_axis("ui_left","ui_right")
@@ -13,4 +17,7 @@ func _physics_process(delta: float) -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
-		print(roundi(rad_to_deg(rotation.y + PI)))
+		degrees = roundi(rad_to_deg(rotation.y + PI))
+		print(degrees)
+		DisplayServer.tts_stop()
+		DisplayServer.tts_speak(str(degrees) + "degrees", voice_id)
