15f067553c135601c5ef24ffcfc9c6aed0099b74
Saph <lc23-0425@lclark.edu>
2026-07-01 10:24:38 -0700

    Adjusted player mesh rotation to reflect input

--- player.gd
+++ player.gd
@@ -9,14 +9,4 @@ func _physics_process(delta: float) -> void:
 	var rotation_input = Input.get_axis("ui_left","ui_right")
 	rotation_direction = (transform.basis * Vector3(0, rotation_input, 0)).normalized()
 	rotation += rotation_direction * ROTATION_SPEED * delta
-
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
