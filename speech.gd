e0f2bb08d9adf68e84c274771465c59e4a80dfde
Saph <lc23-0425@lclark.edu>
2026-07-01 14:18:50 -0700

    Added GodotARIA, replaced tts with screenreader notifications

--- speech.gd
+++ speech.gd
@@ -1,8 +1,7 @@
 extends Node
 
-var voice_id := DisplayServer.tts_get_voices_for_language("en")[0]
-
 func say(text: String, interrupt=false) -> void:
 	if interrupt:
-		DisplayServer.tts_stop()
-	DisplayServer.tts_speak(text, voice_id)
+		GodotARIA.alert_screen_reader(text)
+	else:
+		GodotARIA.notify_screen_reader(text)
