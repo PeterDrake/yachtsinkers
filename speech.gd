47cc672eb89380a279c4e7bcf21cd58b6bb144a0
Peter Drake <drake@lclark.edu>
2026-07-02 09:57:11 -0700

    Switched speech back to TTS

--- speech.gd
+++ speech.gd
@@ -1,8 +1,15 @@
 extends Node
 
+var voice_id := DisplayServer.tts_get_voices_for_language("en")[0]
+
 func say(text: String, interrupt=false) -> void:
-	GodotARIA.debug = true
 	if interrupt:
-		GodotARIA.alert_screen_reader(text)
-	else:
-		GodotARIA.notify_screen_reader(text)
+		DisplayServer.tts_stop()
+	DisplayServer.tts_speak(text, voice_id)
+
+#func say(text: String, interrupt=false) -> void:
+	#GodotARIA.debug = true
+	#if interrupt:
+		#GodotARIA.alert_screen_reader(text)
+	#else:
+		#GodotARIA.notify_screen_reader(text)
