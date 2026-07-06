fb422054a46b1dd7fef2b79a2b8cbf0f658facc7
Peter Drake <drake@lclark.edu>
2026-07-06 11:48:45 -0700

    Removed commented-out code

--- speech.gd
+++ speech.gd
@@ -6,15 +6,3 @@ var voice_id := DisplayServer.tts_get_voices_for_language("en")[0]
 
 func say(text: String, interrupt=false) -> void:
 	caption.text = text
-	#caption.accessibility_name = text
-	#print("Speaking: " + text)
-	#if interrupt:
-		#DisplayServer.tts_stop()
-	#DisplayServer.tts_speak(text, voice_id)
-
-#func say(text: String, interrupt=false) -> void:
-	#GodotARIA.debug = true
-	#if interrupt:
-		#GodotARIA.alert_screen_reader(text)
-	#else:
-		#GodotARIA.notify_screen_reader(text)
