46429b7141b131c4c4e7ebddd392287665b32a9e
Peter Drake <drake@lclark.edu>
2026-07-07 14:43:19 -0700

    Captions effed up

--- yachtsinkers.gd
+++ yachtsinkers.gd
@@ -2,8 +2,13 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Caption.grab_focus()
+	$UI/Caption.grab_focus()
+	get_tree().root.size_changed.connect(on_viewport_size_changed)
+	on_viewport_size_changed()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func on_viewport_size_changed() -> void:
+	print(get_viewport().get_visible_rect().size.x)
+	$UI/Caption.custom_maximum_size = Vector2(get_viewport().get_visible_rect().size.x - 100, -1)
+	#$Caption.custom_maximum_size = Vector2(1000, 100)
 	pass
