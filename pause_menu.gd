f5c9233c90248004e5ec3aeab624476407d4e263
Peter Drake <drake@lclark.edu>
2026-07-15 14:04:31 -0700

    Dummy settings screen can be accessed from pause screen

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,13 +1,16 @@
 extends Control
 
 @onready var yachtsinkers := get_node("..")
-	
+
+var occluded := false  # True if one of the other screens is in front of this
+
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		if visible:
-			unpause()
-		else:
-			pause()
+	if not occluded:
+		if Input.is_action_just_pressed("pause"):
+			if visible:
+				unpause()
+			else:
+				pause()
 
 func pause():
 	print("pause")
@@ -38,7 +41,8 @@ func _on_quit_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
+	occluded = true
+	yachtsinkers.find_child("SettingsMenu").show()
 
 func _on_audio_library_button_pressed() -> void:
 	pass # Replace with function body.
