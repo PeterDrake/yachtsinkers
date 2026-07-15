0b1b63f10c1066b04e1544b87ab0abfbdc86e480
Peter Drake <drake@lclark.edu>
2026-07-15 13:31:30 -0700

    Level is loaded dynamically

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var yachtsinkers := get_node("..")
-
+	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		if visible:
@@ -23,7 +23,7 @@ func _on_visibility_changed() -> void:
 	if visible:
 		$VBoxContainer/HBoxContainer/VBoxContainer/ContinueButton.grab_focus()
 	else:
-		if yachtsinkers:
+		if ready and yachtsinkers:
 			var caption = yachtsinkers.current_level.find_child("Caption")
 			caption.grab_focus()
 
--- yachtsinkers.gd
+++ yachtsinkers.gd
@@ -1,3 +1,7 @@
 extends Node3D
 
-@onready var current_level = $Level
+@onready var current_level
+
+func _ready():
+	current_level = load("res://level.tscn").instantiate()
+	add_child(current_level)
