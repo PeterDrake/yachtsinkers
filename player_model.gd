e09b9eb8ee4eb7d1fd1c7cc4d59cc088822ba29e
Peter Drake <drake@lclark.edu>
2026-07-16 13:48:04 -0700

    Master volume control works

--- player_model.gd
+++ player_model.gd
@@ -1,5 +1,4 @@
 extends Node
 
 func _ready() -> void:
-	print("swim")
 	$AnimationPlayer.play("swim")
--- settings_menu.gd
+++ settings_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var pause_menu = get_node("../PauseMenu")
 
 var previous_screen
+var _bus_index
 
 func _process(_delta: float) -> void:
 	if visible and Input.is_action_just_pressed("pause"):
@@ -22,3 +23,13 @@ func _on_visibility_changed() -> void:
 			$VBoxContainer/Title.grab_focus()
 		else:
 			$VBoxContainer/HBoxContainer/VBoxContainer/MasterSlider.grab_focus()
+
+func _ready() -> void:
+	_bus_index = AudioServer.get_bus_index("Master")
+	var master_slider = $VBoxContainer/HBoxContainer/VBoxContainer/MasterSlider
+	master_slider.value = db_to_linear(AudioServer.get_bus_volume_db(_bus_index))
+	master_slider.value_changed.connect(_on_master_value_changed)
+
+func _on_master_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus_index, linear_to_db(value))
+	$MasterPlayer.play()
