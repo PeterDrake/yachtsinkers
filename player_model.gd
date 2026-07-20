86cfbeac2ce434b6cd2db9a5a1f4cf35bdc03fd1
Saph <lc23-0425@lclark.edu>
2026-07-20 14:53:41 -0700

    Struggled with animation

--- player_model.gd
+++ player_model.gd
@@ -1,6 +1,7 @@
 extends Node
 var movement = ["forward", "left", "right"]
 var wait := false
+var dive := false
 
 func _process(_delta: float) -> void:
 	if get_parent().name != "Player":
@@ -15,14 +16,19 @@ func animate_player():
 		$AnimationPlayer.play("Atack_Anim")
 	if Input.is_action_pressed("dive") and get_parent().dive_enabled:
 		wait = true
+		dive = true
+		get_parent().get_child(0).position -= Vector3.DOWN * 0.03
 		$AnimationPlayer.speed_scale = 2.0
 		$AnimationPlayer.play("Breath_Animm")
+		await get_tree().create_timer(2.0).timeout
+		get_parent().get_child(0).position += Vector3.DOWN * 0.03
 	if Input.is_action_pressed("slap") and get_parent().slap_enabled:
 		wait = true
 		$AnimationPlayer.speed_scale = 2.0
 		$AnimationPlayer.play("TailAtack_Anim")
 	for move in movement:
 		if Input.is_action_just_pressed(move):
+			wait = false
 			$AnimationPlayer.speed_scale = 1.0
 			if get_parent().speed == 250.0:
 				$AnimationPlayer.play("Swim1_Animm")
@@ -32,4 +38,8 @@ func animate_player():
 		$AnimationPlayer.speed_scale = 1.0
 		$AnimationPlayer.play("Idle_Anim")
 	if not $AnimationPlayer.is_playing():
+		print("not playing")
 		wait = false
+		if dive:
+			get_parent().get_child(0).position.y = 0.406
+			dive = false
