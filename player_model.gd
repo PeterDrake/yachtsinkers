72275a3cfde0fa0894f74df35b3581ca499a0c4b
Saph <lc23-0425@lclark.edu>
2026-07-21 14:50:02 -0700

    Finished implementing animations

--- player_model.gd
+++ player_model.gd
@@ -1,7 +1,6 @@
 extends Node
 var movement = ["forward", "left", "right"]
 var wait := false
-var dive := false
 
 func _process(_delta: float) -> void:
 	if get_parent().name != "Player":
@@ -11,35 +10,42 @@ func _process(_delta: float) -> void:
 
 func animate_player():
 	if Input.is_action_pressed("bite") and get_parent().bite_enabled:
-		wait = true
-		$AnimationPlayer.speed_scale = 2.0
-		$AnimationPlayer.play("Atack_Anim")
-	if Input.is_action_pressed("dive") and get_parent().dive_enabled:
-		wait = true
-		dive = true
-		get_parent().get_child(0).position -= Vector3.DOWN * 0.03
-		$AnimationPlayer.speed_scale = 2.0
-		$AnimationPlayer.play("Breath_Animm")
-		await get_tree().create_timer(2.0).timeout
-		get_parent().get_child(0).position += Vector3.DOWN * 0.03
-	if Input.is_action_pressed("slap") and get_parent().slap_enabled:
-		wait = true
-		$AnimationPlayer.speed_scale = 2.0
-		$AnimationPlayer.play("TailAtack_Anim")
+		animate_ability("bite")
+	elif Input.is_action_pressed("dive") and get_parent().dive_enabled:
+		animate_ability("dive")
+	elif Input.is_action_pressed("slap") and get_parent().slap_enabled:
+		animate_ability("slap")
 	for move in movement:
-		if Input.is_action_just_pressed(move):
-			wait = false
-			$AnimationPlayer.speed_scale = 1.0
-			if get_parent().speed == 250.0:
-				$AnimationPlayer.play("Swim1_Animm")
-			else:
-				$AnimationPlayer.play("Swim3_Anim")
+		if Input.is_action_just_pressed(move) and not wait:
+			animate_movement()
+	
+	if not $AnimationPlayer.is_playing():
+		for move in movement:
+			if Input.is_action_pressed(move):
+				animate_movement()
 	if not Input.is_anything_pressed() and not wait:
 		$AnimationPlayer.speed_scale = 1.0
 		$AnimationPlayer.play("Idle_Anim")
-	if not $AnimationPlayer.is_playing():
-		print("not playing")
-		wait = false
-		if dive:
-			get_parent().get_child(0).position.y = 0.406
-			dive = false
+
+func animate_movement():
+	wait = false
+	$AnimationPlayer.speed_scale = 1.0
+	if get_parent().speed == 250.0:
+		$AnimationPlayer.play("Swim1_Animm")
+	else:
+		$AnimationPlayer.play("Swim3_Anim")
+
+func animate_ability(ability: String):
+	wait = true
+	if ability == "bite":
+		$AnimationPlayer.speed_scale = 2.0
+		$AnimationPlayer.play("Atack_Anim")
+	elif ability == "dive":
+		get_parent().get_child(0).position -= Vector3.DOWN * 0.03 #Go down for animation
+		$AnimationPlayer.speed_scale = 2.0
+		$AnimationPlayer.play("Breath_Animm")
+		await get_tree().create_timer(2.0).timeout
+		get_parent().get_child(0).position += Vector3.DOWN * 0.03 #Come back up
+	elif ability == "slap":
+		$AnimationPlayer.speed_scale = 2.0
+		$AnimationPlayer.play("TailAtack_Anim")
