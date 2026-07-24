76a92d577cc74b9f90b671788fc16dda9fae757c
Peter Drake <drake@lclark.edu>
2026-07-24 14:43:13 -0700

    Visual explosions for mines

--- mine.gd
+++ mine.gd
@@ -5,6 +5,7 @@ extends StaticBody3D
 @onready var level := get_node("..")
 
 var exploding := false
+const EXPLOSION_SPEED := 20
 
 func sonar_return() -> void:
 	var distance := position.distance_to(player.position)
@@ -18,7 +19,17 @@ func detonate(hit := true) -> void:
 		if hit:  # Collision with orca rather than tail slap
 			player.take_damage("You hit a mine.")
 		var distance := position.distance_to(player.position)
-		await get_tree().create_timer(distance / 10.0).timeout
+		#await get_tree().create_timer(distance / 10.0).timeout
 		$DetonationSound.play()
 		await get_tree().create_timer(2.0).timeout
 		queue_free()
+
+func _physics_process(delta: float) -> void:
+	if exploding and $Explosion:
+		var x = $Explosion.scale.x
+		x += EXPLOSION_SPEED * delta
+		if x > 3:
+			$Explosion.queue_free()
+			$"FAB converted".queue_free()
+		else:
+			$Explosion.scale = Vector3(x, x, x)
