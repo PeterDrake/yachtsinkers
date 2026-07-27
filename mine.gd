016239d388c3c30cfb92ca66563680c46a8db044
Peter Drake <drake@lclark.edu>
2026-07-27 12:04:19 -0700

    Mine explosions are disabled by default and can be enabled in settings.

--- mine.gd
+++ mine.gd
@@ -3,6 +3,7 @@ extends StaticBody3D
 @onready var speech := get_node("../LevelComponents/Speech")
 @onready var player := get_node("../LevelComponents/Player")
 @onready var level := get_node("..")
+@onready var mine_explosions_enabled_box := get_node("../../SettingsMenu/VBoxContainer/HBoxContainer/VBoxContainer2/MineExplosionBox")
 
 var exploding := false
 const EXPLOSION_SPEED := 20
@@ -20,15 +21,18 @@ func detonate(hit := true) -> void:
 			$CollisionShape3D.disabled = true
 			player.take_damage("You hit a mine.")
 		$DetonationSound.play()
+		$"FAB converted".queue_free()
+		if not mine_explosions_enabled_box.button_pressed:
+			$Explosion.hide()
 		await get_tree().create_timer(2.0).timeout
 		queue_free()
 
 func _physics_process(delta: float) -> void:
-	if exploding and $Explosion.visible:
+	if exploding and mine_explosions_enabled_box.button_pressed and $Explosion.visible:
 		var x = $Explosion.scale.x
 		x += EXPLOSION_SPEED * delta
 		if x > 3:
 			$Explosion.hide()
-			$"FAB converted".queue_free()
+			#$"FAB converted".queue_free()
 		else:
 			$Explosion.scale = Vector3(x, x, x)
